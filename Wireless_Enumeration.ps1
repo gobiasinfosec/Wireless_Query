d146240c40c4921e0b5e4c37b5be8000db4ad771
Tobias Funke <thedavetheory@yahoo.com>
2017-06-13 11:02:55 -0400

    Create Wireless_Enumeration.ps1

---
+++ Wireless_Enumeration.ps1
@@ -0,0 +1,38 @@
+# Retrieve Machine List from AD and then run psexec to gather SSID names and passwords accross a domain
+# Requires Local Admin rights on the machine (Workstation/Domain Admin rights)
+
+Import-Module ActiveDirectory
+
+# Set filepaths and options for variables
+$ou = "*"                                                              # "*" to pull information from all OU's
+$psexec = "C:\PSTools\PsExec.exe"                                      # path to psexec.exe executable
+$output = "C:\temp\wireless_networks.csv"                              # path to output file (will be in csv format)
+
+# Create a new empty array for results
+$results = @()
+
+# Do the lookup and create the output file
+echo "AD-Lookup running, do not close window"
+$workstations = Get-ADComputer -filter {Enabled -eq $true} | Where-Object{$_.DistinguishedName -like $ou } | Select-Object Name
+
+# Iterate through each workstation to get SSID names
+foreach ($workstation in $workstations) {
+    echo "Running lookup on $workstation"
+    $network_ids = & $psexec \\$workstation netsh.exe wlan show all | findstr c:/"SSID name" | %{$_.split('"')[1]} | ? {$_} | sort -uniq
+
+    # Iterate through each SSID name and pull the password
+    foreach ($network_id in $network_ids) {
+        echo "Running lookup on $network_id"
+        $password = & $psexec \\$workstation netsh.exe wlan show profile name="$network_id" key=clear | findstr c:/"Key Content" | %{$_.split('::')[1]}
+        
+        # Write all data to the results array
+        $results += New-Object PsObject -Property @{
+            Workstation = $workstation
+            Password = $password
+            Network = $network_id
+        }
+    }
+}
+
+# Write the results array to a csv file
+$results | convertto-csv -NoTypeInformation -Delimiter "," | % { $_ -replace '"', ""} | out-file $output -Encoding ascii
